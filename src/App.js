729d7575e2669df767e412c3566b5841ce33dc67
desicodeer <144021536+desicodeer@users.noreply.github.com>
2023-12-05 17:56:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 // let name = "Aanchal";
 function App() {
   return (
         <>
         <Navbar title="TextUtils" aboutText="About us"/>
+        <div className="container my-3">
+        <TextForm heading="Enter the Text you like" /> 
+        </div>
         </>
       // <>
       // <nav className="navbar">
