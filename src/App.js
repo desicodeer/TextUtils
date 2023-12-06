42a403bb36da5ca3af7306670c3211d9550c7fdc
desicodeer <144021536+desicodeer@users.noreply.github.com>
2023-12-06 17:51:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,40 @@ import React,{useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 // import About from './components/About';
 // let name = "Aanchal";
 function App() {
    const [mode,setMode] = useState('light');
+   const [alert,setAlert] = useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
    const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor='#212529';
+       showAlert("Dark mode enable","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
+       showAlert("Light mode enable","success");
     }
    }
   return (
         <>
         <Navbar title="TextUtils" aboutText="About us" mode={mode} toggleMode={toggleMode}/>
+         <Alert alert={alert} />
          // <About />
         <div className="container my-3">
-        <TextForm heading="Enter the Text you like" mode={mode} /> 
+        <TextForm heading="Enter the Text you like" mode={mode} showAlert={showAlert}/> 
         </div>
         </>
       // <>
