5afd0813fca654506022e29d4889e0199f781661
desicodeer <144021536+desicodeer@users.noreply.github.com>
2023-12-06 15:57:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 // import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
 // let name = "Aanchal";
 function App() {
+   const [mode,setMode] = useState('light');
+   const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#212529';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+   }
   return (
         <>
-        <Navbar title="TextUtils" aboutText="About us"/>
+        <Navbar title="TextUtils" aboutText="About us" mode={mode} toggleMode={toggleMode}/>
          // <About />
         <div className="container my-3">
-        <TextForm heading="Enter the Text you like" /> 
+        <TextForm heading="Enter the Text you like" mode={mode} /> 
         </div>
         </>
       // <>
