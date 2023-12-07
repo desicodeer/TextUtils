f2c5031bf4738ee6808aeec23352732c96e384c5
desicodeer <144021536+desicodeer@users.noreply.github.com>
2023-12-07 16:25:47 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,6 +34,11 @@ function TextForm(props) {
     navigator.clipboard.writeText(newText.value);
     props.showAlert("Text Copied","success");
   }
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+}
 
   const handleOnChange = (event)=>{
     // console.log("On Change");
@@ -51,9 +56,11 @@ function TextForm(props) {
          <button className="btn btn-light mx-1" onClick={handleCapitalizeClick}>Convert to Capitalise</button>
          <button className="btn btn-success mx-1" onClick={handleClear}>Clear</button>
          <button className="btn btn-secondary mx-1" onClick={handleCopy}>Copy Item</button>
+         <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
          <h2 className="my-3" style={{color: props.mode === 'dark'?'white':'black'}}>Your Text Summary</h2>
          <p style={{color: props.mode === 'dark'?'white':'black'}}><strong>{text.split(" ").length}</strong> words and <strong>{text.length}</strong> characters</p>
          <p style={{color: props.mode === 'dark'?'white':'black'}}>{text.trim().split(/\s+/).filter(word => word !== '').length} words and {text.trim().length} characters</p>
+         <p style={{color: props.mode === 'dark'?'white':'black'}}>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
          <h2 style={{color: props.mode === 'dark'?'white':'black'}}>Preview</h2>
          <p style={{color: props.mode === 'dark'?'white':'black'}}>{text.length>0 ? text : "Enter Something in textbox to preview it"}</p>
        </div>
